8c1e3443cafd716f8b4d21f11b1e4b41fb3155f6
DonPangPang <powertle@outlook.com>
2022-03-30 19:56:12 +0800

    update

--- ExpressionDemos.ConsoleApp3/Program.cs
+++ ExpressionDemos.ConsoleApp3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Linq.Expressions;
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
@@ -12,11 +13,44 @@ var companyNames = new[] {
 };
 
 IQueryable<string> companyNamesSource = companyNames.AsQueryable();
-var fixedQuery = companyNames.OrderBy(x=>x);
+var fixedQuery = companyNames.OrderBy(x => x);
 
-var length = 1;
-var query = companyNamesSource.Select(x=>x.Substring(0, length))
-    .Distinct();
+#region Program1
+// var length = 1;
+// var query = companyNamesSource.Select(x=>x.Substring(0, length))
+//     .Distinct();//去重
 
-length = 2;
-Console.WriteLine(string.Join(", ", query));
+// length = 2;
+// Console.WriteLine(string.Join(", ", query));
+#endregion Program1
+
+#region Program2
+// bool sortByLength = true;
+
+// var query = companyNamesSource;
+
+// if(sortByLength)
+// {
+//     query = query.OrderBy(x=>x.Length);
+// }
+// else
+// {
+//     query = query.OrderByDescending(x=>x.Length);
+// }
+// Console.Write(string.Join(", ", query.ToList()));
+#endregion Program2
+
+#region Program3
+
+string? startsWith = "W";
+string? endsWith = "y";
+
+Expression<Func<string, bool>> expr = (startsWith, endsWith) switch
+{
+    ("" or null, "" or null) => x => true,
+    (_, "" or null) => x => x.StartsWith(startsWith),
+    ("" or null, _) => x => x.EndsWith(endsWith),
+    (_, _) => x => x.StartsWith(startsWith) || x.EndsWith(endsWith)
+};
+
+#endregion Program3
