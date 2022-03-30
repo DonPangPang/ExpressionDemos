160b9dd3e014c3d31cc2e27d4e48e8cc8227d264
DonPangPang <powertle@outlook.com>
2022-03-30 22:07:30 +0800

    update

---
+++ Test.ConsoleApp1/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.ConsoleApp1
+{
+    public class TestClass : ITestInterface
+    {
+        public void TestMethod()
+        {
+            Console.WriteLine("TestClass.TestMethod");
+        }
+    }
+}
